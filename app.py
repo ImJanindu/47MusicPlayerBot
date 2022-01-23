4ecb077a9b846edb703c7b0439b31019fb668093
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2022-01-23 10:18:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,9 @@ SOFTWARE.
 """
 
 import os
+import glob
 import logging
+from yt_dlp import YoutubeDL
 from pytube import YouTube
 from youtube_search import YoutubeSearch
 from pytgcalls import PyTgCalls, idle
@@ -48,7 +50,8 @@ OWNER_ID = int(os.environ["OWNER_ID"])
 
 START_TEXT = """
 Hi <b>{}</b> 👋
-I can play music in Telegram group voice chats. 
+
+I can play music & stream videos in Telegram group voice chats. 
 
 <i>Only my owner can operate me. Make your own bot from the source code.</i>
 """
@@ -152,15 +155,16 @@ async def music_play(_, message):
         link = f"https://youtube.com{results[0]['url_suffix']}"
         thumb = results[0]["thumbnails"][0]
         duration = results[0]["duration"]
+        ytid = link.split("=")[1]
         yt = YouTube(link)
         cap = f"🎵 <b>Playing:</b> [{yt.title}]({link}) \n\n⏳ <b>Duration:</b> {duration}"
-        aud = yt.streams.get_by_itag(140).download()
+        ydl_opts = {'format': 'ba[ext=m4a]'}
+        with YoutubeDL(ydl_opts) as ydl:
+            ydl.download([link])
+        
+        aud = glob.glob(f"*{ytid}*")[0]
     except Exception as e:
-        if "Too Many Requests" in str(e):
-            await m.edit("❗️<i>Please wait at least 30 seconds to use me.</i>")
-            os.system(f"kill -9 {os.getpid()} && python3 app.py")
-        else:
-            return await m.edit(str(e))
+        return await m.edit(str(e))
     
     try:
         if str(chat_id) in CHATS:
