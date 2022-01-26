f0126af6f19ebafea60c741c6cc530e037df535f
Janindu Malshan <75658442+ImJanindu@users.noreply.github.com>
2022-01-26 18:06:00 +0530

    Create admin_check.py

---
+++ helpers/admin_check.py
@@ -0,0 +1,21 @@
+import functools
+
+def is_admin(func):
+    @functools.wraps(func)
+    async def oops(client,message):
+        is_admin = False
+        try:
+            user = await message.chat.get_member(message.from_user.id)
+            admin_strings = ("creator", "administrator")
+            if user.status not in admin_strings:
+                is_admin = False
+            else:
+                is_admin = True
+
+        except ValueError:
+            is_admin = True
+        if is_admin:
+            await func(client,message)
+        else:
+            await message.reply("Only admins can execute this command!")
+    return oops
